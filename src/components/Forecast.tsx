947afb406f948b2d1ea4274f560a6240e9913d0c
Tamás Kovács <kovacs.tamas0408@gmail.com>
2025-09-24 17:09:45 +0200

    Create component for forecast data

---
+++ src/components/Forecast.tsx
@@ -0,0 +1,35 @@
+import Accordion from "./Accordion";
+import ForecastData from "./ForecastData";
+import { useWeatherData } from "../hooks/useWeatherData";
+import { type GeoResponse } from "../type";
+
+function Forecast({ data, defaultCity = "Budapest" }: { data: GeoResponse | null; defaultCity?: string }) {
+  const cityName = data?.name || defaultCity;
+  const { weather: weatherData, isLoading } = useWeatherData(cityName);
+
+  if (isLoading) return <div className='loading-container'>Loading...</div>;
+  if (!weatherData) return <div className='error-container'>No data</div>;
+
+  const dailyData = weatherData.daily;
+
+  return (
+    <section className='forecast-wrapper'>
+      {dailyData.time.map((date, i) => (
+        <Accordion
+          key={date}
+          date={date}
+          tempMin={dailyData.temperature_2m_min[i]}
+          tempMax={dailyData.temperature_2m_max[i]}
+          className='accordion--compact'
+        >
+          <ForecastData
+            precipitation={dailyData.precipitation_sum[i]}
+            windSpeedMax={dailyData.wind_speed_10m_max[i]}
+          />
+        </Accordion>
+      ))}
+    </section>
+  );
+}
+
+export default Forecast;
