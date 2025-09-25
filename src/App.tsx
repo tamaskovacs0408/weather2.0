537fd2ab8a9fe803b7930323064442bb5f8e1c6b
Tamás Kovács <kovacs.tamas0408@gmail.com>
2025-09-25 10:52:22 +0200

    Refactor App component: Enhance structure and add QueryClient setup for React Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,43 @@
-import './App.scss'
+import "./App.scss";
+import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import CitySearch from "./components/CitySearch";
+import CurrentWeather from "./components/CurrentWeather";
+import Forecast from "./components/Forecast";
 
-function App() {
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      refetchOnWindowFocus: false,
+      staleTime: 1000 * 60 * 5,
+      gcTime: 1000 * 60 * 30,
+    },
+  },
+});
 
-  return (
-    <>
-      
-    </>
-  )
+declare global {
+  interface Window {
+    __TANSTACK_QUERY_CLIENT__: import("@tanstack/query-core").QueryClient;
+  }
 }
 
-export default App
+function App() {
+  const [geoData, setGeoData] = useState<GeoResponse | null>(null);
+  function handleCitySelect(data: GeoResponse) {
+    setGeoData(data);
+  }
+  return (
+    <QueryClientProvider client={queryClient}>
+      <main>
+        <CitySearch onSelect={handleCitySelect} />
+        <CurrentWeather data={geoData} defaultCity='Budapest' />
+        <Forecast data={geoData} defaultCity='Budapest' />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </main>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
