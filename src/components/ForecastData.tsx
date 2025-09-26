4c0aa91655ec4e9d0a319557d180775071d42456
Tamás Kovács <kovacs.tamas0408@gmail.com>
2025-09-26 15:46:16 +0200

    Refactor ForecastData component structure for improved readability

--- src/components/ForecastData.tsx
+++ src/components/ForecastData.tsx
@@ -1,30 +1,33 @@
 import { formatDate } from "../utils/dateFormatHelper";
 import { type ForecastDayProps } from "../type";
-import "../styles/ForecastData.scss"
+import "../styles/ForecastData.scss";
 
-function ForecastData({ precipitation, windSpeedMax, sunRise, sunSet}: ForecastDayProps) {
-    return (
-        <section className="forecast-data-wrapper">
-            <div className="precipitation-container">
-                <h3>Precipitation:</h3>
-                <span>{precipitation} mm</span>
-            </div>
-            <div className="wind-container">
-                <h3>Wind speed:</h3>
-                <span>{windSpeedMax} km/h</span>
-            </div>
-            <div className="solar-data-wrapper">
-                <div className="sunrise-container">
-                    <h3>Sunrise</h3>
-                    <span>{formatDate(sunRise)}</span>
-                </div>
-                <div className="sunset-container">
-                    <h3>Sunset</h3>
-                    <span>{formatDate(sunSet)}</span>
-                </div>
-            </div>
-        </section>
-    )
+function ForecastData({
+  precipitation,
+  windSpeedMax,
+  sunRise,
+  sunSet,
+}: ForecastDayProps) {
+  return (
+    <section className='forecast-data-wrapper'>
+      <div className='precipitation-container'>
+        <h3>Precipitation:</h3>
+        <span>{precipitation} mm</span>
+      </div>
+      <div className='wind-container'>
+        <h3>Wind speed:</h3>
+        <span>{windSpeedMax} km/h</span>
+      </div>
+      <div className='sunrise-container'>
+        <h3>Sunrise</h3>
+        <span>{formatDate(sunRise)}</span>
+      </div>
+      <div className='sunset-container'>
+        <h3>Sunset</h3>
+        <span>{formatDate(sunSet)}</span>
+      </div>
+    </section>
+  );
 }
 
 export default ForecastData;
