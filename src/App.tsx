de3bb30f4b9087f06e158ddca843fbb2687b8001
Tamás Kovács <kovacs.tamas0408@gmail.com>
2025-09-27 16:54:03 +0200

    feat: add night mode functionality based on the current hour

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.scss";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import CitySearch from "./components/CitySearch";
@@ -25,13 +25,20 @@ declare global {
 }
 
 function App() {
+  const [isNight, setIsNight] = useState(false);
   const [geoData, setGeoData] = useState<GeoResponse | null>(null);
   function handleCitySelect(data: GeoResponse) {
     setGeoData(data);
   }
+
+  useEffect(() => {
+    const hour = new Date().getHours();
+    setIsNight(hour >= 18 || hour < 6)
+  },[])
+
   return (
     <QueryClientProvider client={queryClient}>
-      <main>
+      <main className={isNight ? 'night' : 'day'}>
         <CitySearch onSelect={handleCitySelect} />
         <CurrentWeather data={geoData} defaultCity='Budapest' />
         <Forecast data={geoData} defaultCity='Budapest' />
